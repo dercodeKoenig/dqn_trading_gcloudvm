f9396071b0b21e19701feef9d469d19560ed6346
MrToffel <marvineckhardt04092003@gmail.com>
2024-01-05 13:40:16 +0100

    Update dqn_training_tpu.py

--- t3/dqn_training_tpu.py
+++ t3/dqn_training_tpu.py
@@ -13,16 +13,16 @@ import time
 
 
 
-batch_size = 128
+batch_size = 32
 gamma = 0.995
 learning_rate=0.00001
 
 num_model_inputs = 2+5+3+1
 n_actions = 2
 m1 = np.eye(n_actions, dtype="float32")
-num_data_generation_threads = 12
-batch_generation_threads = 4
-memory_size = 120_000
+num_data_generation_threads = 8
+batch_generation_threads = 6
+memory_size = 80_000
 batch_q_size = 512
 data_q_maxlen = 128
 ep_len = 100
