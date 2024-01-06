ea8f469ae4ef7a26dbab196d3a5b07ba9926c61d
MrToffel <marvineckhardt04092003@gmail.com>
2024-01-06 17:20:47 +0100

    .

--- t4/make_model.py
+++ t4/make_model.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 #config
 batch_size = 128
-gamma = 0.975
-learning_rate=0.0000001
+gamma = 0.95
+learning_rate=0.00001
 num_data_generation_threads = 12
 batch_generation_threads = 8
 memory_size = 300_000
@@ -148,12 +148,12 @@ def make_model():
   actions_all = tf.keras.layers.LeakyReLU()(actions_all)
   actions_all = tf.keras.layers.Dense(128)(actions_all)
   actions_all = tf.keras.layers.LeakyReLU()(actions_all)
-  actions_all = tf.keras.layers.Dense(256)(actions_all)
+  actions_all = tf.keras.layers.Dense(128)(actions_all)
   actions_all = tf.keras.layers.LeakyReLU()(actions_all)
   #actions_all = TransformerBlock(actions_all.shape[-1], 4, 256)(actions_all)
   #actions_all = TransformerBlock(actions_all.shape[-1], 4, 256)(actions_all)
   #actions_all = TransformerBlock(actions_all.shape[-1], 4, 256)(actions_all)
-  actions_all = TransformerBlock(actions_all.shape[-1], 4, 256)(actions_all)
+  actions_all = TransformerBlock(actions_all.shape[-1], 2, 256)(actions_all)
   #actions_all = tf.keras.layers.GRU(1024)(actions_all)
   actions_all = tf.keras.layers.GlobalAveragePooling1D()(actions_all)
 
