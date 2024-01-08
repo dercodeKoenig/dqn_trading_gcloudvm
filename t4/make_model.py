e130dadbc9ea70f0a56fd778763497060ae79c0c
MrToffel <marvineckhardt04092003@gmail.com>
2024-01-08 10:06:14 +0100

    .

--- t4/make_model.py
+++ t4/make_model.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 #config
-batch_size = 128
+batch_size = 32
 gamma = 0.995
 learning_rate=0.00001
 num_data_generation_threads = 12
@@ -39,7 +39,7 @@ class TransformerBlock(layers.Layer):
 
 def make_model():
 
-  embed_times = tf.keras.layers.Embedding(60*24*7,8)
+  embed_times = tf.keras.layers.Embedding(60*24,8)
   embed_pda_types = tf.keras.layers.Embedding(7,6)
 
   pd_arrays_m1_input = tf.keras.layers.Input(shape = (10,3))
@@ -137,6 +137,9 @@ def make_model():
 
   input_closing_prices = tf.keras.layers.Input(shape=(1))
   input_current_pos = tf.keras.layers.Input(shape=(1))
+  input_current_day_in = tf.keras.layers.Input(shape=(1))
+  input_closing_day = tf.keras.layers.Embedding(7,3)(input_current_day_in)
+  input_closing_day = tf.keras.layers.Flatten()(input_closing_day)
   input_closing_times_in = tf.keras.layers.Input(shape=(1))
   input_closing_times = embed_times(input_closing_times_in)
   input_closing_times = tf.keras.layers.Flatten()(input_closing_times)
@@ -167,7 +170,7 @@ def make_model():
   actions_m15 = tf.keras.layers.GRU(64)(actions_m15)
   #actions_m15 = tf.keras.layers.GlobalAveragePooling1D()(actions_m15)
 
-  dense_input = tf.keras.layers.Concatenate()([input_current_pos, input_closing_prices, input_closing_times, pda_list_m60, pda_list_d1, pda_list_m15, pda_list_m5, pda_list_m1, actions_m1, actions_m5, actions_m15])
+  dense_input = tf.keras.layers.Concatenate()([input_current_pos, input_closing_prices, input_current_day_in, input_closing_times, pda_list_m60, pda_list_d1, pda_list_m15, pda_list_m5, pda_list_m1, actions_m1, actions_m5, actions_m15])
   
 
   x = tf.keras.layers.Dense(512)(dense_input)
@@ -182,5 +185,5 @@ def make_model():
 
   x = tf.keras.layers.Dense(2, activation = "linear")(x)
 
-  model = tf.keras.Model(inputs = [input_current_pos, input_closing_prices, input_closing_times_in, pd_arrays_m1_input, pd_arrays_m5_input, pd_arrays_m15_input, pd_arrays_m60_input, pd_arrays_d1_input, action_m1_inputs, action_m5_inputs, action_m15_inputs], outputs=x)
+  model = tf.keras.Model(inputs = [input_current_pos, input_closing_prices, input_current_day_in, input_closing_times_in, pd_arrays_m1_input, pd_arrays_m5_input, pd_arrays_m15_input, pd_arrays_m60_input, pd_arrays_d1_input, action_m1_inputs, action_m5_inputs, action_m15_inputs], outputs=x)
   return model
